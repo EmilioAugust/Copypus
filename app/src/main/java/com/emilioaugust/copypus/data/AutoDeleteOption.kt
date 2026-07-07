7a6dcd0d576ecbe29e1d6a4c44067b4e994238bf
EmilioAugust <kuluev.emil.8@gmail.com>
2026-07-07 12:09:42 +0500

    Optimized code

--- app/src/main/java/com/emilioaugust/copypus/data/AutoDeleteOption.kt
+++ app/src/main/java/com/emilioaugust/copypus/data/AutoDeleteOption.kt
@@ -1,12 +1,15 @@
 package com.emilioaugust.copypus.data
 
-enum class AutoDeleteOption(val title: String) {
-    NEVER("Never"),
-    ONE_HOUR("1 hour"),
-    FIVE_HOURS("5 hours"),
-    ONE_DAY("1 day"),
-    ONE_WEEK("1 week"),
-    ONE_MONTH("1 month");
+import androidx.annotation.StringRes
+import com.emilioaugust.copypus.R
+
+enum class AutoDeleteOption(@StringRes val titleRes: Int) {
+    NEVER(R.string.never_ad_option),
+    ONE_HOUR(R.string._1_hour_ad_option),
+    FIVE_HOURS(R.string._5_hours_ad_option),
+    ONE_DAY(R.string._1_day_ad_option),
+    ONE_WEEK(R.string._1_week_ad_option),
+    ONE_MONTH(R.string._1_month_ad_option);
 
     fun toMillis(): Long {
         return when (this) {
